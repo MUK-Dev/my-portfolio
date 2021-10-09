c83941952f1e2e1aabce0d52bc5dcbf9d7f43922
MUK-Dev <manian1221@gmail.com>
2021-10-09 08:27:50 +0500

    Added animation on load

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,13 @@
+import "materialize-css/dist/css/materialize.min.css";
+
+import "../styles/_app.css";
+
 function MyApp({ Component, pageProps }) {
-	return <Component {...pageProps} />;
+	return (
+		<div className="app">
+			<Component {...pageProps} />
+		</div>
+	);
 }
 
 export default MyApp;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,40 @@
+import React, { useEffect, useState } from "react";
+
+import s from "../styles/index.module.css";
+import { OnLoadAnimation } from "@components/animations";
+import { motion, AnimatePresence } from "framer-motion";
+
+const home = {
+	hidden: {
+		opacity: 0,
+	},
+	show: {
+		opacity: 1,
+	},
+};
+
 export default function Home() {
-	return <div>Home Page</div>;
+	const [animation, stopAnimation] = useState(true);
+
+	const body = animation ? (
+		<motion.div
+			key="animate-on-load"
+			onAnimationComplete={() => stopAnimation(false)}
+		>
+			<OnLoadAnimation
+				animation={animation}
+				stopAnimation={() => stopAnimation(false)}
+			/>
+		</motion.div>
+	) : (
+		<motion.div variants={home} initial="hidden" animate="show">
+			Home Page
+		</motion.div>
+	);
+
+	return (
+		<div className={s.root}>
+			<AnimatePresence exitBeforeEnter>{body}</AnimatePresence>
+		</div>
+	);
 }
